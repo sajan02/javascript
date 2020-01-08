861796f65cf11373747cdca74127f8a435e94f53
SajanKashi <sajan.kashi@nineleaps.com>
2020-01-08 14:01:57 +0530

    Sajan - Module

---
+++ module.js
@@ -0,0 +1,17 @@
+var myModule = (function() {
+    'use strict';
+ 
+    var _privateProperty = 'Hello World';
+     
+    function _privateMethod() {
+        console.log(_privateProperty);
+    }
+     
+    return {
+        publicMethod: function() {
+            _privateMethod();
+        }
+    };
+}());
+
+myModule.publicMethod();
